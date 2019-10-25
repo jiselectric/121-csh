ec920d98a4f6136decb66d070c450c4df08188f0
jiselectric9320 <jiselectric9320@gmail.com>
2019-10-25 21:58:57 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app class="grey lighten-4">
+    <navbar />
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+
+  </v-app>
+</template>
+
+<script>
+import navbar from '@/components/navbar'
+
+export default {
+  name: 'App',
+  components: {
+    navbar
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,52 @@
+<template>
+  <nav>
+    <div>
+      <v-app-bar>
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title class="text-uppercase black--text">
+          <span class="font-weight-light"> 121 CSH </span>
+          <span> Integrated Management System </span>
+        </v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon> exit_to_app </v-icon>
+        </v-btn>
+      </v-app-bar>
+    </div>
+
+    <v-navigation-drawer permanent>
+    <v-list-item
+      v-for="item in items"
+      v-model="drawer"
+      :key="item.title"
+      link
+    >
+
+    <v-list-item-icon>
+      <v-icon> {{item.icon}} </v-icon>
+    </v-list-item-icon>
+
+    <v-list-item-content>
+      <v-list-item-title> {{item.title}} </v-list-item-title>
+    </v-list-item-content>
+    </v-list-item>
+  </v-navigation-drawer>
+
+  </nav>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
