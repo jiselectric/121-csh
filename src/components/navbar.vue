357b069178a72d40bf32328f4d8379840babedb1
jiselectric9320 <jiselectric9320@gmail.com>
2019-10-26 16:53:35 +0900

    second commit - navigation drawer

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -2,7 +2,7 @@
   <nav>
     <div>
       <v-app-bar>
-        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
         <v-toolbar-title class="text-uppercase black--text">
           <span class="font-weight-light"> 121 CSH </span>
           <span> Integrated Management System </span>
@@ -14,26 +14,28 @@
           <v-icon> exit_to_app </v-icon>
         </v-btn>
       </v-app-bar>
+
+      <v-navigation-drawer v-model="drawer" class="white"
+        height="700"
+        width="300"
+        >
+        <v-list>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      </v-navigation-drawer>
     </div>
-
-    <v-navigation-drawer permanent>
-    <v-list-item
-      v-for="item in items"
-      v-model="drawer"
-      :key="item.title"
-      link
-    >
-
-    <v-list-item-icon>
-      <v-icon> {{item.icon}} </v-icon>
-    </v-list-item-icon>
-
-    <v-list-item-content>
-      <v-list-item-title> {{item.title}} </v-list-item-title>
-    </v-list-item-content>
-    </v-list-item>
-  </v-navigation-drawer>
-
   </nav>
 </template>
 
@@ -41,7 +43,14 @@
 export default {
   data () {
     return {
-      drawer: false
+      drawer: false,
+      items: [
+        { title: 'Announcement', icon: 'announcement' },
+        { title: 'Submit', icon: 'cloud_upload' },
+        { title: 'Calendar', icon: 'event' },
+        { title: 'Detail', icon: 'search' },
+        { title: 'About', icon: 'question_answer' }
+      ]
     }
   }
 }
